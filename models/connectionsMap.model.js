ab740862a10c8cfe958fa7b2d8a14b2f37185b44
Prashnik <thebrahmosztech43@gmail.com>
2025-08-26 18:12:22 +0530

    Save connection updated

--- models/connectionsMap.model.js
+++ models/connectionsMap.model.js
@@ -28,21 +28,29 @@ const connectionsMapSchema = new mongoose.Schema(
 
 )
 
+// This method saves the left or right swipe of a user (interested or ignored)
 
 connectionsMapSchema.statics.saveConnection = async function (toUserId, fromUserId, status) {
 
+    const allowedStatuses = ["ignored", "interested"];
     try {
         if (toUserId.toString() === fromUserId.toString()) {
             throw new Error("Can't send connection request to yourself");
         }
-        const existingConnection = await this.findOne({ toUserId, fromUserId });
-        if (existingConnection) {
-            existingConnection.status = status;
-            return await existingConnection.save();
-        } else {
-            const newConnection = new this({ toUserId, fromUserId, status });
-            return await newConnection.save();
+        if (!allowedStatuses.includes(status)) {
+            throw new Error("Bad Request: Status must be ignored or interested");
         }
+        const existingConnection = await this.findOne({
+            $or: [
+                { toUserId, fromUserId },
+                { toUserId: fromUserId, fromUserId: toUserId }
+            ]
+        });
+        if (existingConnection) {
+            throw new Error("Connection request already exists between these users");
+        }
+        const newConnection = new this({ toUserId, fromUserId, status });
+        return await newConnection.save();
     } catch (error) {
         throw new Error(error.message);
     }
